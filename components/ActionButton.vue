dd04ccd33eaf4ce6eeec0d5c5efc30f4f7d224e9
Larry <kaul.lawrence@gmail.com>
2023-07-16 11:23:31 -0700

    fixed entry log

--- components/ActionButton.vue
+++ components/ActionButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="button  my-2 ">{{text}}</button>
+    <button class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center mr-2 mb-2 ">{{text}}</button>
 </template>
 
 <script setup>
@@ -17,5 +17,6 @@ defineProps({
     color: #FFFFFF;
     height: 55px;
 }
+
 
 </style>
